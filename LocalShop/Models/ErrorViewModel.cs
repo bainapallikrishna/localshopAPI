284986163325cebf6e24e195f239e8a278622460
bainapalli krishna <bainapallikrishna201@gmail.com>
2025-05-21 12:42:24 +0530

    Add project files.

---
+++ LocalShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LocalShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

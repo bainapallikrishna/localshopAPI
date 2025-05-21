49f5d1c8645e4e874ea7d2c9f117bcf57b80f69c
bainapalli krishna <bainapallikrishna201@gmail.com>
2025-05-21 18:57:18 +0530

    first commit

---
+++ LocalShop/API/LoginController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LocalShop.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+    }
+}

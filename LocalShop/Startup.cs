79ffae90d8886dc4499e728dbedf788d7503f752
bainapalli krishna <bainapallikrishna201@gmail.com>
2025-08-08 17:06:09 +0530

    added startup file

---
+++ LocalShop/Startup.cs
@@ -0,0 +1,93 @@
+using LocalShop.Domain.Interfaces;
+using LocalShop.Infrastructure.Data;
+using LocalShop.Infrastructure.Repositories;
+using LocalShop.Middleware;
+using LocalShop.Services;
+using LocalShop.Services.Mapping;
+using LocalShop.Services.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Identity.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.  
+builder.Services.AddControllersWithViews();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "LocalApp",
+        Version = "v1",
+        Description = "Local Shop"
+    });
+});
+
+builder.Services.AddDbContext<LocalShopDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnLocalShopDb"),
+         sqlOptions => sqlOptions.MigrationsAssembly("LocalShop.Infrastructure")
+    ));
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ProductService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IJwtService, JwtService>();
+
+builder.Services.AddAutoMapper(cfg => cfg.AddProfile<AutoMapperProfile>());
+
+
+
+builder.Services.AddControllers();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        RoleClaimType = ClaimTypes.Role,
+        IssuerSigningKey = new SymmetricSecurityKey(
+        Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+})
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.  
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseMiddleware<GlobalExceptionMiddleware>();
+app.UseAuthentication();
+
+app.UseAuthorization();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapControllerRoute(
+   name: "default",
+   pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
